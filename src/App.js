ac04255264f3dc92caa73dc8275825d578274ad0
Kevin Chambers <kevinchambers@Kevins-MacBook-Air.local>
2019-04-05 15:44:20 +1100

    Added credits. Final commit?

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 	      	<h1>REACTMAN</h1>
 	        <Grid />
         </div>
+        <p>Try using keyboard to control! You can use the arrow buttons next to the joystick too. Created by <a href="https://kevinchambers.net">Kevin Chambers</a>. Code available at <a href="https://github.com/kchambers0/pacman" target="_blank" rel="noopener noreferrer">github</a>.</p>
       </div>
     );
   }
